fea7dd962e0290afeefbc7bfc0c674d57f25266c
Florian Bacher <florian.bacher@dynatrace.com>
2024-04-26 11:31:58 +0200

    adapt example workflow, support keptn task executions

--- pkg/executor/executor.go
+++ pkg/executor/executor.go
@@ -21,12 +21,21 @@ import (
 )
 
 const (
-	apiVersion               = "v1beta1"
+	apiVersion               = "v1"
 	keptnMetricsResourceName = "keptnmetrics"
+	keptnTasksResourceName   = "keptntasks"
 	analysisResourceName     = "analyses"
 
 	paramKeptnQuery = "query"
+	paramStart      = "start"
+	paramEnd        = "end"
 )
+
+type taskStatus struct {
+	createdResource schema.GroupVersionResource
+	name            string
+	namespace       string
+}
 
 var keptnMetricsResource = schema.GroupVersionResource{
 	Group:    "metrics.keptn.sh",
@@ -40,10 +49,18 @@ var analysisResource = schema.GroupVersionResource{
 	Resource: analysisResourceName,
 }
 
+var taskResource = schema.GroupVersionResource{
+	Group:    "lifecycle.keptn.sh",
+	Version:  apiVersion,
+	Resource: keptnTasksResourceName,
+}
+
 type queryObject struct {
 	GroupVersionResource schema.GroupVersionResource
 	ResourceName         string
 	DurationString       string
+	Start                string
+	End                  string
 	Namespace            string
 	Arguments            map[string]interface{}
 }
@@ -51,12 +68,19 @@ type queryObject struct {
 type queryResult struct {
 	Details string
 	Result  string
+	Start   string
+	End     string
+	Status  *taskStatus
+	Requeue bool
 }
 
 type Executor struct {
 	token           string
 	client          dynamic.Interface
 	analysisTimeout time.Duration
+	taskTimeout     time.Duration
+
+	workflowExecutions map[string]*taskStatus
 }
 
 func New(cfg *rest.Config, token string) (*Executor, error) {
@@ -68,9 +92,11 @@ func New(cfg *rest.Config, token string) (*Executor, error) {
 		return nil, fmt.Errorf("could not initialize Executor: %w", err)
 	}
 	return &Executor{
-		token:           token,
-		client:          client,
-		analysisTimeout: 15 * time.Second,
+		token:              token,
+		client:             client,
+		analysisTimeout:    15 * time.Second,
+		taskTimeout:        5 * time.Minute,
+		workflowExecutions: map[string]*taskStatus{},
 	}, nil
 }
 
@@ -89,6 +115,8 @@ func (e *Executor) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	slog.Log(r.Context(), slog.LevelInfo, string(body))
+
 	templateArgs := &argoexecutor.ExecuteTemplateArgs{}
 
 	err = json.Unmarshal(body, templateArgs)
@@ -101,9 +129,17 @@ func (e *Executor) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// get the parameters
 
 	query := ""
+	start := ""
+	end := ""
 	for _, param := range templateArgs.Template.Inputs.Parameters {
 		if param.Name == paramKeptnQuery {
 			query = param.GetValue()
+		}
+		if param.Name == paramStart {
+			start = param.GetValue()
+		}
+		if param.Name == paramEnd {
+			end = param.GetValue()
 		}
 	}
 	if query == "" {
@@ -118,6 +154,9 @@ func (e *Executor) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+
+	queryObj.Start = start
+	queryObj.End = end
 
 	var res *queryResult
 	switch queryObj.GroupVersionResource.Resource {
@@ -136,30 +175,66 @@ func (e *Executor) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
+	case keptnTasksResourceName:
+		if status, ok := e.workflowExecutions[templateArgs.Workflow.ObjectMeta.Uid]; ok {
+			res, err = e.checkForTaskStatus(queryObj, context.Background(), status.name, status.namespace)
+		} else {
+			res, err = e.executeKeptnTask(queryObj)
+		}
+		if err != nil {
+			slog.Log(r.Context(), slog.LevelError, fmt.Sprintf("Could not execute Analysis query: %v", err))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 	default:
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	reply := &argoexecutor.ExecuteTemplateReply{
-		Node: &wfv1.NodeResult{
-			Phase:   wfv1.NodeSucceeded,
-			Message: fmt.Sprintf("Result of %s: %s", query, res.Result),
-			Outputs: &wfv1.Outputs{
-				Parameters: []wfv1.Parameter{
-					{
-						Name:  "result",
-						Value: wfv1.AnyStringPtr(res.Result),
-					},
-					{
-						Name:  "details",
-						Value: wfv1.AnyStringPtr(res.Details),
-					},
-				},
-				Result: &res.Details,
+	var reply *argoexecutor.ExecuteTemplateReply
+	if res.Requeue {
+		reply = &argoexecutor.ExecuteTemplateReply{
+			Node: &wfv1.NodeResult{
+				Phase:    wfv1.NodeRunning,
+				Message:  fmt.Sprintf("Executing %s", query),
+				Progress: wfv1.Progress("0/1"),
 			},
-			Progress: wfv1.Progress("1/1"),
-		},
+			Requeue: &v1.Duration{
+				Duration: 5 * time.Second,
+			},
+		}
+		if res.Status != nil {
+			e.workflowExecutions[templateArgs.Workflow.ObjectMeta.Uid] = res.Status
+		}
+	} else {
+		reply = &argoexecutor.ExecuteTemplateReply{
+			Node: &wfv1.NodeResult{
+				Phase:   wfv1.NodeSucceeded,
+				Message: fmt.Sprintf("Result of %s: %s", query, res.Result),
+				Outputs: &wfv1.Outputs{
+					Parameters: []wfv1.Parameter{
+						{
+							Name:  "result",
+							Value: wfv1.AnyStringPtr(res.Result),
+						},
+						{
+							Name:  "details",
+							Value: wfv1.AnyStringPtr(res.Details),
+						},
+						{
+							Name:  "start",
+							Value: wfv1.AnyStringPtr(res.Start),
+						},
+						{
+							Name:  "end",
+							Value: wfv1.AnyStringPtr(res.End),
+						},
+					},
+					Result: &res.Details,
+				},
+				Progress: wfv1.Progress("1/1"),
+			},
+		}
 	}
 
 	marshal, err := json.Marshal(reply)
@@ -178,6 +253,15 @@ func (e *Executor) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func (e *Executor) queryKeptnAnalysis(obj *queryObject) (*queryResult, error) {
+
+	timeFrame := map[string]string{}
+
+	if obj.Start != "" && obj.End != "" {
+		timeFrame["from"] = obj.Start
+		timeFrame["to"] = obj.End
+	} else {
+		timeFrame["recent"] = obj.DurationString
+	}
 	analysis := &unstructured.Unstructured{
 		Object: map[string]interface{}{
 			"apiVersion": fmt.Sprintf("metrics.keptn.sh/%s", apiVersion),
@@ -190,10 +274,8 @@ func (e *Executor) queryKeptnAnalysis(obj *queryObject) (*queryResult, error) {
 				"analysisDefinition": map[string]interface{}{
 					"name": obj.ResourceName,
 				},
-				"timeframe": map[string]interface{}{
-					"recent": obj.DurationString,
-				},
-				"args": obj.Arguments,
+				"timeframe": timeFrame,
+				"args":      obj.Arguments,
 			},
 		},
 	}
@@ -283,6 +365,86 @@ func (e *Executor) queryKeptnMetric(queryObj *queryObject) (*queryResult, error)
 	return nil, fmt.Errorf("could not retrieve KeptnMetric - no value found in resource %s/%s", queryObj.Namespace, queryObj.ResourceName)
 }
 
+func (e *Executor) executeKeptnTask(obj *queryObject) (*queryResult, error) {
+	task := &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": fmt.Sprintf("lifecycle.keptn.sh/%s", apiVersion),
+			"kind":       "KeptnTask",
+			"metadata": map[string]interface{}{
+				"name":      fmt.Sprintf("%s-%s", obj.ResourceName, uuid.New().String()[:6]),
+				"namespace": obj.Namespace,
+			},
+			"spec": map[string]interface{}{
+				"taskDefinition": obj.ResourceName,
+				"parameters":     obj.Arguments,
+				// these context attributes are required
+				"context": map[string]string{
+					"appName":         "",
+					"appVersion":      "",
+					"objectType":      "",
+					"taskType":        "",
+					"workloadName":    "",
+					"workloadVersion": "",
+				},
+			},
+		},
+	}
+
+	// set the timeout to 10s - this will give Keptn enough time to reconcile the Analysis
+	// and store the result in the status of the resource created here.
+	ctx, cancel := context.WithTimeout(context.Background(), e.taskTimeout)
+	defer cancel()
+
+	createdTask, err := e.client.
+		Resource(taskResource).
+		Namespace(obj.Namespace).
+		Create(ctx, task, v1.CreateOptions{})
+
+	if err != nil {
+		return nil, fmt.Errorf("could not create Keptn Analysis %s/%s: %w", obj.Namespace, obj.ResourceName, err)
+	}
+
+	res := &queryResult{
+		Requeue: true,
+		Status: &taskStatus{
+			createdResource: taskResource,
+			name:            createdTask.GetName(),
+			namespace:       createdTask.GetNamespace(),
+		},
+	}
+	return res, nil
+
+}
+
+func (e *Executor) checkForTaskStatus(obj *queryObject, ctx context.Context, name, namespace string) (*queryResult, error) {
+
+	get, err := e.client.Resource(taskResource).Namespace(namespace).Get(ctx, name, v1.GetOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("could not check status of created Keptn Task %s/%s: %w", obj.Namespace, obj.ResourceName, err)
+	}
+	// check completion by checking end time
+	endTimeStr, ok, err := unstructured.NestedString(get.Object, "status", "endTime")
+	if ok && endTimeStr != "" {
+		startTimeStr, _, err := unstructured.NestedString(get.Object, "status", "startTime")
+		if err != nil {
+			return nil, fmt.Errorf("could not check status of created Keptn Task %s/%s: %w", obj.Namespace, obj.ResourceName, err)
+		}
+		statusStr, _, err := unstructured.NestedString(get.Object, "status", "status")
+		if err != nil {
+			return nil, fmt.Errorf("could not check status of created Keptn Task %s/%s: %w", obj.Namespace, obj.ResourceName, err)
+		}
+		res := &queryResult{
+			Result: statusStr,
+			Start:  startTimeStr,
+			End:    endTimeStr,
+		}
+		return res, nil
+	}
+	return &queryResult{
+		Requeue: true,
+	}, nil
+}
+
 func parseQuery(query string) (*queryObject, error) {
 	result := &queryObject{}
 	// sanitize the query by converting to lower case, trimming spaces and line break characters
@@ -326,6 +488,20 @@ func parseQuery(query string) (*queryObject, error) {
 				}
 			}
 		}
+	case "keptntask", keptnTasksResourceName:
+		result.GroupVersionResource = taskResource
+
+		if len(split) >= 4 {
+			args := strings.Split(split[3], ";")
+
+			for i := 0; i < len(args); i++ {
+				keyValue := strings.Split(args[i], "=")
+				if len(keyValue) == 2 {
+					result.Arguments[keyValue[0]] = keyValue[1]
+				}
+			}
+		}
+		break
 
 	default:
 		return nil, errors.New("unexpected resource kind provided in the query. must be one of: ['keptnmetric', 'analysis']")
